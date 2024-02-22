3cf7e95cd7998934e13851d7b74045ab560da7fe
Emily Louie <emilyslouie@gmail.com>
2024-02-22 00:48:36 -0500

    Get random colour and random palette from http request

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -55,10 +56,21 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // for loading images
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    // for making http calls
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
+    // for (de)serialization of JSON
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    // for viewmodel utilities
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
