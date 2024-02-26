aba99616a1be47df50c6c0ecd71ab0aff54aa4e8
Victoria Li <v42li@uwaterloo.ca>
2024-02-26 14:48:26 -0500

    Create screens for home, library and preview with navigation menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     // for viewmodel utilities
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    // for navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
