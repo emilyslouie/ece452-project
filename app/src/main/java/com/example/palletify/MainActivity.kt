ff5876bb2646fbf13bb8f68a08e0af403410805a
Padmaashini Sukumaran <padmaashini.s@gmail.com>
2024-02-27 13:03:45 -0500

    Setup Preview screen with some UI elements

--- app/src/main/java/com/example/palletify/MainActivity.kt
+++ app/src/main/java/com/example/palletify/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.palletify.ui.generator.GeneratorScreen
+import com.example.palletify.ui.preview.PreviewScreen
 import com.example.palletify.ui.theme.PalletifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +18,8 @@ class MainActivity : ComponentActivity() {
             PalletifyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                   GeneratorScreen();
+//                   GeneratorScreen();
+                    PreviewScreen();
                 }
             }
         }
