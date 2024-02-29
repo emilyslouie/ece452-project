ab42f207f06b66d21ecdd6fac331794ebd828a29
Zihe <z958zhan@uwaterloo.ca>
2024-02-29 11:47:10 -0500

    working camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,18 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
+    // for activity results
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    // for CameraX
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/palletify/ui/CameraViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.palletify.ui
+
+class CameraViewModel {
+}
