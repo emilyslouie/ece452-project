3d5d99133932661a61971d0d411c402f2b1a68c5
Zihe <z958zhan@uwaterloo.ca>
2024-03-01 13:26:07 -0500

    improved photo capturing

---
+++ app/src/main/java/com/example/palletify/ui/image/createImageFile.kt
@@ -0,0 +1,18 @@
+package com.example.palletify.ui.image
+
+import android.content.Context
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+
+fun Context.createImageFile(): File {
+    // Create an image file name
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val imageFileName = "JPEG_" + timeStamp + "_"
+    val image = File.createTempFile(
+        imageFileName, /* prefix */
+        ".jpg", /* suffix */
+        externalCacheDir      /* directory */
+    )
+    return image
+}
