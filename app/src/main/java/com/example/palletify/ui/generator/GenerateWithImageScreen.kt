a1b0fb7596eb032eba294e77d831c51a72024360
Zihe <z958zhan@uwaterloo.ca>
2024-03-27 01:19:15 -0400

    use mutableStateListOf for colors

--- app/src/main/java/com/example/palletify/ui/generator/GenerateWithImageScreen.kt
+++ app/src/main/java/com/example/palletify/ui/generator/GenerateWithImageScreen.kt
@@ -9,12 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,16 +26,25 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
+import com.example.palletify.R
 import com.example.palletify.database.PaletteViewModel
+import java.lang.Long.parseLong
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,6 +56,9 @@ fun GenerateWithImageScreen(
     val context = LocalContext.current
     val paletteViewModel =
         ViewModelProvider(context as ViewModelStoreOwner).get(PaletteViewModel::class.java)
+
+    val colors = remember { mutableStateListOf(0xFFfff7b2, 0xFFccf865, 0xFF49cc6e, 0xFF9b7367, 0xFF775798)};
+    val otherColors = remember { mutableStateListOf(0xFFfff7b2, 0xFFccf865)};
 
     Scaffold(
         topBar = {
@@ -69,51 +85,37 @@ fun GenerateWithImageScreen(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                Button(
-                    modifier = Modifier
-                        .padding(top = 8.dp)
-                        .heightIn(min = 55.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFfff7b2)),
-                    onClick = { /* No action is triggered */ }
-                ) {
-                    Text("")
+                colors.forEach { color ->
+                    Button(
+                        modifier = Modifier
+                            .padding(top = 8.dp)
+                            .heightIn(min = 55.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(color)),
+                        onClick = { /* No action is triggered */ }
+                    ) {
+                        Text("")
+//                        Icon(
+//                            Icons.Filled.Delete,
+//                            contentDescription = "Localized description"
+//                        )
+                    }
                 }
-                Button(
-                    modifier = Modifier
-                        .padding(top = 8.dp)
-                        .heightIn(min = 55.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFccf865)),
-                    onClick = { /* No action is triggered */ }
-                ) {
-                    Text("")
+                if (colors.size <= 5) {
+                    Button(
+                        modifier = Modifier
+                            .padding(top = 8.dp)
+                            .heightIn(min = 55.dp),
+                        onClick = { colors.add(0xFFfff7b2) }
+                    ) {
+                        Text(
+                            text = "+",
+                            style = TextStyle(
+                                fontSize = 32.sp, fontWeight = FontWeight.Bold
+                            )
+                        )
+                    }
                 }
-                Button(
-                    modifier = Modifier
-                        .padding(top = 8.dp)
-                        .heightIn(min = 55.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF49cc6e)),
-                    onClick = { /* No action is triggered */ }
-                ) {
-                    Text("")
-                }
-                Button(
-                    modifier = Modifier
-                        .padding(top = 8.dp)
-                        .heightIn(min = 55.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF9b7367)),
-                    onClick = { /* No action is triggered */ }
-                ) {
-                    Text("")
-                }
-                Button(
-                    modifier = Modifier
-                        .padding(top = 8.dp)
-                        .heightIn(min = 55.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF775798)),
-                    onClick = { /* No action is triggered */ }
-                ) {
-                    Text("")
-                }
+
             }
             Button(modifier = Modifier
                 .padding(12.dp)
