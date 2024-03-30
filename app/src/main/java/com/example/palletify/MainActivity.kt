01d442abaab45a3df09b70f9e8f12368ff22872b
Zihe <zhangzihe@gmail.com>
2024-03-29 23:46:17 -0400

    fix nav

--- app/src/main/java/com/example/palletify/MainActivity.kt
+++ app/src/main/java/com/example/palletify/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.palletify
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
@@ -22,10 +27,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -36,7 +47,10 @@ import com.example.palletify.ui.preview.PreviewScreen
 import com.example.palletify.ui.theme.PalletifyTheme
 import kotlinx.coroutines.launch
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.palletify.ui.generator.GenerateWithImageScreen
+import com.example.palletify.ui.image.createImageFile
 import com.example.palletify.ui.preview.PreviewViewModel
+import java.util.Objects
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -67,18 +81,90 @@ fun NavDrawer() {
     val context = LocalContext.current.applicationContext
     val previewViewModel: PreviewViewModel = viewModel()
 
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        BuildConfig.APPLICATION_ID + ".provider", file
+    )
+
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+    var uploadedImageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+    val cameraLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+            capturedImageUri = uri
+        }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    val selectImageLauncher = rememberLauncherForActivityResult(contract =
+    ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uploadedImageUri = uri
+    }
+
     ModalNavigationDrawer(
         drawerState = drawerState,
         gesturesEnabled = true,
         drawerContent = {
             ModalDrawerSheet {
-                NavigationDrawerItem(label = { Text(text = "Home") },
+                NavigationDrawerItem(label = { Text(text = "Generate") },
                     selected = false,
                     onClick = {
                         coroutineScope.launch {
                             drawerState.close()
                         }
-                        navigationController.navigate(Screens.Home.screen) {
+                        navigationController.navigate(Screens.GenerateScreen.screen) {
+                            popUpTo(0)
+                        }
+                    })
+                NavigationDrawerItem(label = { Text(text = "Capture") },
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+
+                        val permissionCheckResult =
+                            ContextCompat.checkSelfPermission(
+                                context,
+                                android.Manifest.permission.CAMERA
+                            )
+                        if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                            cameraLauncher.launch(uri)
+                        } else {
+                            // Request a permission
+                            permissionLauncher.launch(android.Manifest.permission.CAMERA)
+                        }
+
+                        navigationController.navigate(Screens.Capture.screen) {
+                            popUpTo(0)
+                        }
+                    })
+                NavigationDrawerItem(label = { Text(text = "Upload") },
+                    selected = false,
+                    onClick = {
+                        uploadedImageUri = null
+
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+
+                        selectImageLauncher.launch("image/*")
+
+                        navigationController.navigate(Screens.Upload.screen) {
                             popUpTo(0)
                         }
                     })
@@ -130,14 +216,22 @@ fun NavDrawer() {
         ) {
             NavHost(
                 navController = navigationController,
-                startDestination = Screens.Home.screen
+                startDestination = Screens.GenerateScreen.screen
             ) {
-                composable(Screens.Home.screen) { HomeScreen(navigationController) }
+                composable(Screens.Capture.screen) {
+                    if (capturedImageUri.path?.isNotEmpty() == true) {
+                        GenerateWithImageScreen(capturedImageUri)
+                    }
+                }
+                composable(Screens.Upload.screen) {
+                    if (uploadedImageUri?.path?.isNotEmpty() == true) {
+                        GenerateWithImageScreen(uploadedImageUri)
+                    }
+                }
                 composable(Screens.Library.screen) { Library(context, navigationController) }
                 composable(Screens.PreviewScreen.screen) { PreviewScreen(previewViewModel) }
                 composable(Screens.GenerateScreen.screen) { GeneratorScreen() }
             }
         }
-
     }
 }
