bc49e8e99373cf071e241ebaa8e6984aa465971e
Lokhin Wu <lokhin.wu@live.com>
2024-03-30 18:50:32 -0400

    save this or that palettes

--- app/src/main/java/com/example/palletify/ui/this_or_that/ThisOrThatScreen.kt
+++ app/src/main/java/com/example/palletify/ui/this_or_that/ThisOrThatScreen.kt
@@ -1,6 +1,7 @@
 package com.example.palletify.ui.this_or_that
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
@@ -35,8 +38,10 @@ import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.palletify.ColorUtils.hexToComposeColor
+import com.example.palletify.database.Palette
 import com.example.palletify.database.PaletteViewModel
 import com.example.palletify.ui.components.TrademarkComponentWrapper
+import com.example.palletify.ui.generator.calculateLuminosity
 import com.example.palletify.ui.theme.PalletifyTheme
 import kotlin.concurrent.thread
 
@@ -66,8 +71,9 @@ fun ThisOrThatScreen(thisOrThatViewModel: ThisOrThatViewModel = viewModel()) {
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 16.dp, bottom = 16.dp),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
+                            .padding(top = 16.dp, bottom = 16.dp, start = 8.dp, end = 8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
                         // This button
                         Button(onClick = {
@@ -75,7 +81,32 @@ fun ThisOrThatScreen(thisOrThatViewModel: ThisOrThatViewModel = viewModel()) {
                         }) {
                             Text(
                                 text = "This",
-                                style = typography.titleLarge
+                                style = typography.titleSmall
+                            )
+                        }
+                        TextButton(
+                            onClick = {
+                                val currentPalette = thisOrThatViewModel.uiState.value.currentPalettes[0].colors
+                                val numberOfColors = thisOrThatViewModel.uiState.value.currentPalettes[0].numberOfColours
+                                val mode = thisOrThatViewModel.uiState.value.currentPalettes[0].mode
+                                val colorsList = mutableListOf<String>()
+                                for (i in 0 until numberOfColors) {
+                                    val color = currentPalette[i].hex.value
+                                    colorsList.add(color)
+                                }
+                                val palette = Palette(
+                                    0,
+                                    numberOfColors,
+                                    colorsList,
+                                    mode.mode,
+                                    favourite = false
+                                )
+                                paletteViewModel.addPalette(palette)
+                            }
+                        ) {
+                            Text(
+                                text = "Save",
+                                style = typography.labelSmall
                             )
                         }
                         Text(text = "or")
@@ -85,10 +116,34 @@ fun ThisOrThatScreen(thisOrThatViewModel: ThisOrThatViewModel = viewModel()) {
                         }) {
                             Text(
                                 text = "That",
-                                style = typography.titleLarge
+                                style = typography.titleSmall
                             )
                         }
-
+                        TextButton(
+                            onClick = {
+                                val currentPalette = thisOrThatViewModel.uiState.value.currentPalettes[1].colors
+                                val numberOfColors = thisOrThatViewModel.uiState.value.currentPalettes[1].numberOfColours
+                                val mode = thisOrThatViewModel.uiState.value.currentPalettes[1].mode
+                                val colorsList = mutableListOf<String>()
+                                for (i in 0 until numberOfColors) {
+                                    val color = currentPalette[i].hex.value
+                                    colorsList.add(color)
+                                }
+                                val palette = Palette(
+                                    0,
+                                    numberOfColors,
+                                    colorsList,
+                                    mode.mode,
+                                    favourite = false
+                                )
+                                paletteViewModel.addPalette(palette)
+                            }
+                        ) {
+                            Text(
+                                text = "Save",
+                                style = typography.labelSmall
+                            )
+                        }
                     }
                 }
             }
@@ -156,13 +211,32 @@ fun ColorsInRow(
         ) {
             Row(
                 modifier = Modifier
-                    .weight(1f)
                     .fillMaxWidth(0.5f)
                     .height(heightPerColor)
                     .background(hexToComposeColor(firstColor.hex.value.toString()))
                     .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
             ) {
+                val luminosity =
+                    calculateLuminosity(firstColor.rgb.r, firstColor.rgb.g, firstColor.rgb.b)
 
+                Column(
+                    modifier = Modifier
+                        .clickable { }
+                ) {
+                    Text(
+                        modifier = Modifier.padding(bottom = 2.dp),
+                        text = firstColor.hex.clean,
+                        color = if (luminosity >= 0.179) Color.Black else Color.White,
+                        style = typography.headlineSmall
+                    )
+                    Text(
+                        text = firstColor.name.value,
+                        color = if (luminosity >= 0.179) Color(35, 35, 35) else Color(230, 230, 230),
+                        style = typography.labelMedium
+                    )
+                }
             }
         }
 
@@ -171,15 +245,34 @@ fun ColorsInRow(
             list = thisOrThatViewModel.trademarkedColor.hexes,
             map = thisOrThatViewModel.trademarkedColor.colorsMap
         ) {
-            Column(
+            Row(
                 modifier = Modifier
-                    .weight(1f)
                     .fillMaxWidth(1f)
                     .height(heightPerColor)
                     .background(hexToComposeColor(secondColor.hex.value.toString()))
                     .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
             ) {
+                val luminosity =
+                    calculateLuminosity(secondColor.rgb.r, secondColor.rgb.g, secondColor.rgb.b)
 
+                Column(
+                    modifier = Modifier
+                        .clickable { }
+                ) {
+                    Text(
+                        modifier = Modifier.padding(bottom = 2.dp),
+                        text = secondColor.hex.clean,
+                        color = if (luminosity >= 0.179) Color.Black else Color.White,
+                        style = typography.headlineSmall
+                    )
+                    Text(
+                        text = secondColor.name.value,
+                        color = if (luminosity >= 0.179) Color(35, 35, 35) else Color(230, 230, 230),
+                        style = typography.labelMedium
+                    )
+                }
             }
         }
     }
